527df2c003989120a8ef349cd25d7fe872eec80c
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-08 15:49:36 +0100

    Generators creating custom greetings based on the age, gender and name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import react from "react";
 
 function App() {
+  //accessing the states of all the inputs + answer
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  //function that changes the name
+  const changeInName = (e) => {
+    setName(e.target.value);
+    console.log(name);
+  };
+
+  //function that changes the age
+  const changeInAge = (e) => {
+    setAge(e.target.value);
+  };
+
+  //function that changes the gender
+  const changeInGender = (e) => {
+    setGender(e.target.value);
+  };
+
+  //function that changes the answer based on the provided information
+  const generateAnswer = (e) => {
+    e.preventDefault();
+    if (name == "" || age == "") {
+      setAnswer("Both name and age must be filled.");
+    } else if (gender == "male") {
+      setAnswer(`Hello, Mr. ${name}.`);
+    } else if (gender == "other") {
+      setAnswer(`Hello, ${name}`);
+    } else {
+      if (age > 30) {
+        setAnswer(`Hello, Ms. ${name}.`);
+      } else {
+        setAnswer(`Hello, Miss ${name}.`);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Please provide us information so we can sell it to advertisers</p>
+      <h1>{answer}</h1>
+      <form onSubmit={generateAnswer}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={changeInName}
+        ></input>
+        <input
+          type="number"
+          value={age}
+          placeholder="age"
+          onChange={changeInAge}
+        ></input>
+        <select value={gender} onChange={changeInGender}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <button>Send data</button>
+      </form>
+    </>
   );
 }
 
